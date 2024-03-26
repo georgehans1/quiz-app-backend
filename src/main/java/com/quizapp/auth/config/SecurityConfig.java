8a91125eaf59c4c83217caa58ef1555304d32540
George Hanson <george.hanson@turntabl.io>
2024-03-26 23:04:49 +0000

    debug cookie issue

--- src/main/java/com/quizapp/auth/config/SecurityConfig.java
+++ src/main/java/com/quizapp/auth/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
 
     private static CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://hans-quizapp.web.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://hans-quizapp.web.app","https://hans-quizapp.firebaseapp.com"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
